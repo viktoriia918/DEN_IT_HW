295f9dea0d5c293dd26c2870192266d20997f1aa
Viktoriia Stasiuk <vikkussya22@gmail.com>
2025-07-07 20:02:44 +0200

    java program home

---
+++ home_work1/src/main/java/java_hw_2/task_1/Main.java
@@ -0,0 +1,13 @@
+package java_hw_2.task_1;
+
+public class Main {
+    public static void main(String[] args){
+    String string1= "This line that i want to cut,cause it is too long";
+    String string2= string1.substring(0,35); //"This line that i want to cut,cause"
+    String string3= string2.replace("thet i want to cut","that don't want to cut")+"it is perfect";
+        System.out.println("string1: " + string1 + " (length: " + string1.length() + ")");
+        System.out.println("string2: " + string2 + " (length: " + string2.length() + ")");
+        System.out.println("string3: " + string3 + " (length: " + string3.length() + ")");
+
+    }
+}
