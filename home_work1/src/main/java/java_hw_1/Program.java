b987ab5f86ce57e9c2e98400fbb2704fcc25c9fd
Viktoriia Stasiuk <vikkussya22@gmail.com>
2025-07-12 12:25:49 +0200

    java hw1 code

--- home_work1/src/main/java/java_hw_1/Program.java
+++ home_work1/src/main/java/java_hw_1/Program.java
@@ -4,9 +4,9 @@ public class Program {
     public static void main(String[] args) {
         int a = 7;
         int b = 9;
-        a = a + b; // после a = 16
-        b = a - b; // после b = 7
-        a = a - b; // после a = 9
+        a = a + b;
+        b = a - b;
+        a = a - b;
         System.out.println("a ="+ a);
         System.out.println("b ="+ b);
 
