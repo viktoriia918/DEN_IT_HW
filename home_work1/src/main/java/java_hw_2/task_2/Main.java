22d47732f48c07dbd1288ae0e433050aaa0c75bc
Viktoriia Stasiuk <vikkussya22@gmail.com>
2025-07-12 12:35:22 +0200

    java hw2 code

--- home_work1/src/main/java/java_hw_2/task_2/Main.java
+++ home_work1/src/main/java/java_hw_2/task_2/Main.java
@@ -4,12 +4,9 @@ public class Main{
 
 public static void main(String[] args) {
     String text = "Completely random text in English. In it, we just need to determine how man times the character 'a' occurs there. And we can use the split method and the length method.";
-
     String lowerCaseText = text.toLowerCase();
-
     String[] parts = lowerCaseText.split("a");
     int count = parts.length - 1;
-
     System.out.println("Кількість входжень символу 'a' або 'A': " + count);
 }
 }
--- home_work1/src/main/java/java_hw_2/task_3/Main.java
+++ home_work1/src/main/java/java_hw_2/task_3/Main.java
@@ -3,20 +3,16 @@ package java_hw_2.task_3;
 public class Main {
     public static void main(String[] args) {
         String string = "Testing, is my favourite job";
-
         String[] words = string.replace(",", "").split(" ");
-
         for (int i = 0; i < words.length; i++) {
-            System.out.println("Слово" + (i + 1) + " = " + words[i] +
-                    ", Довжина цього слова = " + words[i].length());
+        System.out.println("Слово" + (i + 1) + " = " + words[i] + ", Довжина цього слова = " + words[i].length());
         }
         int firstLength = words[0].length();
         boolean isLongest = true;
-
         for (int i = 1; i < words.length; i++) {
-            if (words[i].length() >= firstLength) {
-                isLongest = false;
-                break;
+        if (words[i].length() >= firstLength) {
+        isLongest = false;
+        break;
             }
         }
 
