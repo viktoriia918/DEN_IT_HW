78f417ae201d5426b983006c7743298d763bbf36
Viktoriia Stasiuk <vikkussya22@gmail.com>
2025-07-26 16:42:46 +0200

    java hw5 code

---
+++ home_work1/src/main/java/java_hw_5/NumbersApplication.java
@@ -0,0 +1,27 @@
+package java_hw_5;
+
+import java.util.Random;
+
+public class NumbersApplication {
+    private String name;
+    private int gameNumber;
+
+    public NumbersApplication(String name) {
+        this.name = name;
+        Random random = new Random();
+        this.gameNumber = random.nextInt(101); // [0-100]
+    }
+
+    public boolean checkGuess(int userGuess) {
+        if (userGuess < gameNumber) {
+            System.out.println("Your number is too small. Please, try again.");
+            return false;
+        } else if (userGuess > gameNumber) {
+            System.out.println("Your number is too big. Please, try again.");
+            return false;
+        } else {
+            System.out.println("Congratulations, " + name + "!");
+            return true;
+        }
+    }
+}
