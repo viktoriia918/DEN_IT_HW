bfac0122f3f7ef8c8e673bdc05d23a96f6fa82de
Viktoriia Stasiuk <vikkussya22@gmail.com>
2025-07-26 17:09:53 +0200

    java hw4 code

--- home_work1/src/main/java/java_hw_4/task_5/Main.java
+++ home_work1/src/main/java/java_hw_4/task_5/Main.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] array = new int[10];
         Random random = new Random();
-        for (int i = 0; i < array.length; i++){
-        array[i] = random.nextInt(100);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
         }
         System.out.print("array:");
-        for (int num : array ){
-        System.out.print(num + " ");
+        for (int num : array) {
+            System.out.print(num + " ");
         }
         System.out.println();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number to search:");
         int userNumber = scanner.nextInt();
         boolean found = false;
-        for (int num : array){
-        if(num == userNumber){
-        found = true;
-        break;
+        for (int num : array) {
+            if (num == userNumber) {
+                found = true;
+                break;
+            }
         }
-        }
-        if (found){
-        System.out.println("Number" + userNumber + "Is in in the array.");
-        System.out.println("Number" + userNumber + "Not in array.");
+        if (found) {
+            System.out.println("Number" + userNumber + "Is in in the array.");
+            System.out.println("Number" + userNumber + "Not in array.");
         }
         scanner.close();
     }
--- home_work1/src/main/java/java_hw_4/task_6/Main.java
+++ home_work1/src/main/java/java_hw_4/task_6/Main.java
@@ -1,29 +1,30 @@
 package java_hw_4.task_6;
 
 import java.util.Random;
+
 public class Main {
-    public static void main(String[] args){
-    int[] array = new int[48];
-    Random random = new Random();
-    for (int i = 0; i < array.length; i++) {
-    array[i] = random.nextInt(101) - 50;
-    }
-    System.out.print("Array: ");
-    for (int num : array) {
-    System.out.print(num + " ");
-    }
-    System.out.println();
-    int min = array[0];
-    int max = array[0];
-    for (int i = 1; i < array.length; i++) {
-    if (array[i] < min) {
-    min = array[i];
-    }
-    if (array[i] > max) {
-    max = array[i];
-    }
-    }
-    System.out.println("Maximum number: " + min);
-    System.out.println("Minimum number: " + max);
+    public static void main(String[] args) {
+        int[] array = new int[48];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(101) - 50;
+        }
+        System.out.print("Array: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        int min = array[0];
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println("Maximum number: " + min);
+        System.out.println("Minimum number: " + max);
     }
 }
